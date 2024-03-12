f62503d3c2c2f6124e0fedd4b32648bd5ab6bce8
Pavel <pasha-senin@mail.ru>
2024-03-12 17:51:33 +0300

    for pythonanywhere

--- hw2/dbproject/dbapp/models.py
+++ hw2/dbproject/dbapp/models.py
@@ -8,12 +8,14 @@ class Client(models.Model):
     email = models.EmailField()
     address = models.TextField()
 
+from django.db import models
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    category = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, default='Uncategorized')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to='product_images/')
+    image = models.ImageField(upload_to='product_images/', default='product_images/default.jpg')  # Используйте свой путь к изображению-заглушке
 
 class Order(models.Model):
     STATUS_CHOICES = [
@@ -22,15 +24,15 @@ class Order(models.Model):
         ('delivered', 'Delivered'),
     ]
 
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product, through='OrderItem')
+    client = models.ForeignKey('Client', on_delete=models.CASCADE)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='processing')
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     order_date = models.DateTimeField(auto_now_add=True)
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     item_total = models.DecimalField(max_digits=10, decimal_places=2)
+
 
--- hw2/dbproject/dbproject/settings.py
+++ hw2/dbproject/dbproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-biglv(vim%n$&nq1yax1h-cgt!&rqep$zmmhwfc7yx4-l3t_3e'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'testing3011.pythonanywhere.com',
+]
 
 
 # Application definition
@@ -77,7 +83,14 @@ WSGI_APPLICATION = 'dbproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'testing3011$default',
+        'USER': 'testing3011',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'testing3011.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4'; SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -117,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
